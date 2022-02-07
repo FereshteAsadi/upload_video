4c28c6cd11c82556ee7c61d91e0605f3cdb619ed
FereshteAsadi <fereshte.baniasadii@gmail.com>
2022-02-07 09:41:48 +0330

    Create the UsersRepository

--- app/Http/Controllers/Web/AuthController.php
+++ app/Http/Controllers/Web/AuthController.php
@@ -2,10 +2,30 @@
 
 namespace App\Http\Controllers\Web;
 
-use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Http\Requests\LoginRequest;
+use App\Http\Controllers\Controller;
+use App\Repositories\UsersRepository;
+use App\Http\Requests\RegisterRequest;
+
 
 class AuthController extends Controller
 {
+    public function __construct(UsersRepository $users)
+    {
+        $this->USERS = $users ;
 
+    }
+    public function register(RegisterRequest $request )
+    {
+
+    }
+    public function logIn(LoginRequest $request)
+    {
+
+    }
+    public function logOut()
+    {
+
+    }
 }
