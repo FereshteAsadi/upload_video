c43b490518f3093a4e9b89e4dfe3a7e3c3deeed4
FereshteAsadi <fereshte.baniasadii@gmail.com>
2022-02-07 10:37:52 +0330

    Complete the login and home view

---
+++ resources/views/home.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=o, initial-scale=1.0">
+    <link rel="stylesheet" href="{{ URL::asset('css/home.css') }}">
+    <title>Document</title>
+</head>
+<body>
+    <header>
+        <div class="conteinerheader">
+            <div class="tabs">
+                <input type="radio" id="radio1" name="tabs">
+                 <label for="radio1" class="tab"><a href="home.html">home</a> </label>
+                <input type="radio" id="radio2" name="tabs">
+                <label for="radio2" class="tab" ><a href="login.html">login</a></label>
+                <input type="radio" id="radio3" name="tabs">
+                <label for="radio3" class="tab"><a href="singup.html">singup</a></label>
+                <input type="radio" id="radio4" name="tabs">
+                <label for="radio4" class="tab"><a href="checkvideo.html">video</a></label>
+                <span class="glider"></span>
+            </div>
+        </div>
+    </header>
+</body>
+</html>
--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,1 +1,39 @@
-resources/views/login.blade.php
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="{{ URL::asset('css/login.css'); }}
+    " type="text/css">
+
+
+    <title>Document</title>
+</head>
+<body>
+    <header>
+        @include('home');
+    </header>
+    <div class="container">
+        <p class="heading"> login in  </p>
+        <form action="">
+            <div class="box">
+                <p> Email </p>
+                <div>
+                    <input type="email" name=" " id="" placeholder="Enter Your Email">
+                </div>
+            </div>
+            <div class="box">
+                <p>Password</p>
+                <div>
+                    <input type="password" name="" id="" placeholder="Enter Your Password">
+                </div>
+            </div>
+            <!-- <input type="button" value="login" class="loginBtn"> -->
+            <button class="loginBtn">login</button>
+            <p class="text">Dont Have an account? <a href="singup.html">sing up </a></p>
+        </form>
+    </div>
+</body>
+</html>
